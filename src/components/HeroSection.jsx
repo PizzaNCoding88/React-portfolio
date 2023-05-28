66ab0c478d41e9a95c5ff3eb4685e08107342b72
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-05-28 19:23:01 +0100

    hero section started

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return(
+    <>
+    <div className='h-screen flex justify-center items-center bg-primary font-NotoSerif relative'>
+        <div className='before:content-["UN"] before:text-[15rem] before:text-additional before:absolute before:top-[50%] before:left-[50%] before:-translate-x-1/2 before:-translate-y-1/2 before:font-bold before:z-0 before:drop-shadow-lg before:blur-[1px]'></div>
+        <div className='w-4/5 z-20'>
+            <p className='text-left mb-5 text-secondary font-bold text-xl'>Frontend</p>
+            <h1 className='text-center text-5xl uppercase leading-10 font-bold text-secondary drop-shadow-2xl'>Umberto</h1>
+            <p className='text-right mt-5 text-secondary font-bold text-xl'>Web Dev</p>
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default HeroSection
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#EDEDE4",
+      secondary: "#F74906",
+      tertiary: "#554E44",
+      additional: "#FFF9F9",
+    },
+    fontFamily: {
+      NotoSerif: ["Noto Serif", "serif"],
+    },
     extend: {},
   },
   plugins: [],
