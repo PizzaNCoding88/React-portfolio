fab19b7bef6efc025539ccf15bcdad275d7794fc
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-06-08 09:32:58 +0100

    animated images

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -20,7 +20,7 @@ const Presentation = () => {
       <section className="bg-gradient-to-r from-primary to-[#222222] h-screen">
         <div className="w-4/5 mx-auto flex justify-center flex-col h-full">
           <h1 className="text-secondary text-center text-2xl font-bold">
-            Building the web my way
+            Building the web the way I like it
           </h1>
           <h2 className=" text-slate-400 opacity-90 text-center mt-12 font-extralight">
             Umberto Nardiello
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import data from "../data/projectData";
-
+import { motion } from "framer-motion";
 const Projects = () => {
   return (
     <>
@@ -12,11 +12,19 @@ const Projects = () => {
               id="projects"
             >
               <a href={data.href} target="_blank" rel="noreferrer">
-                <img
+                <motion.img
                   src={data.source}
                   alt=""
                   className="w-4/5 mx-auto rounded-md"
-                ></img>
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  transition={{ duration: 1 }}
+                  variants={{
+                    visible: { opacity: 1, x: 0 },
+                    hidden: { opacity: 0, x: -160 },
+                  }}
+                ></motion.img>
                 <p className="text-secondary w-4/5 mx-auto uppercase text-center mt-2 font-bold">
                   {data.title}
                 </p>
@@ -33,3 +41,91 @@ const Projects = () => {
 };
 
 export default Projects;
+
+// const imgVariants = {
+//   // visible: { x: "0%", transition: { duration: 1 } },
+//   // hidden: { x: "-150%" },
+//   visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+//   hidden: { opacity: 0, scale: 0.5 },
+// };
+
+// const Projects = () => {
+//   const imgControls = useAnimation();
+//   const [ref, inView] = useInView();
+//   useEffect(() => {
+//     if (inView) {
+//       imgControls.start("visible");
+//     }
+//   }, [imgControls, inView]);
+//   return (
+//     <>
+//       <div className="bg-gradient-to-r from-primary to-[#222222]">
+//         {data.map((data) => {
+//           return (
+//             <div
+//               key={data.id}
+//               className={`w-full ${data.id === 1 ? "pt-8" : "mt-8"}`}
+//               id="projects"
+//             >
+//               <a href={data.href} target="_blank" rel="noreferrer">
+//                 <motion.img
+//                   src={data.source}
+//                   alt="project"
+//                   className="w-4/5 mx-auto rounded-md"
+//                   variants={imgVariants}
+//                   initial="hidden"
+//                   animate={imgControls}
+//                   ref={ref}
+//                 ></motion.img>
+//                 <p className="text-secondary w-4/5 mx-auto uppercase text-center mt-2 font-bold">
+//                   {data.title}
+//                 </p>
+//                 <p className="text-secondary w-4/5 mx-auto text-center text-sm mt-4 font-thin">
+//                   {data.description}
+//                 </p>
+//               </a>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Projects;
+
+// import data from "../data/projectData";
+
+// const Projects = () => {
+//   return (
+//     <>
+//       <div className="bg-gradient-to-r from-primary to-[#222222]">
+//         {data.map((data) => {
+//           return (
+//             <div
+//               key={data.id}
+//               className={`w-full ${data.id === 1 ? "pt-8" : "mt-8"}`}
+//               id="projects"
+//             >
+//               <a href={data.href} target="_blank" rel="noreferrer">
+//                 <img
+//                   src={data.source}
+//                   alt=""
+//                   className="w-4/5 mx-auto rounded-md"
+//                 ></img>
+//                 <p className="text-secondary w-4/5 mx-auto uppercase text-center mt-2 font-bold">
+//                   {data.title}
+//                 </p>
+//                 <p className="text-secondary w-4/5 mx-auto text-center text-sm mt-4 font-thin">
+//                   {data.description}
+//                 </p>
+//               </a>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Projects;
