3a5da1e290f1982e96646406b02a78dcadabbb5c
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-06-09 00:00:40 +0100

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/PizzaNCoding88/React-portfolio/tree/gh-pages",
   plugins: [react()],
 });
