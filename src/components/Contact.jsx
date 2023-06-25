c72d976c174d76c4a2352892b35365706dfcca6d
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-06-25 17:21:06 +0100

    form completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,107 +1,134 @@
-import TextareaAutosize from "react-textarea-autosize";
+// import TextareaAutosize from "react-textarea-autosize";
 import React from "react";
 
 const Contact = () => {
-  // const [result, setResult] = React.useState("");
+  const [result, setResult] = React.useState("");
 
-  // const onSubmit = async (event) => {
-  //   event.preventDefault();
-  //   setResult("Sending....");
-  //   const formData = new FormData(event.target);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
 
-  //   formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+    formData.append("access_key", "4359a0b1-14fd-4278-b15b-c45673a1d9dc");
 
-  //   const res = await fetch("https://api.web3forms.com/submit", {
-  //     method: "POST",
-  //     body: formData,
-  //   }).then((res) => res.json());
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    }).then((res) => res.json());
 
-  //   if (res.success) {
-  //     console.log("Success", res);
-  //     setResult(res.message);
-  //   } else {
-  //     console.log("Error", res);
-  //     setResult(res.message);
-  //   }
-  // };
+    if (res.success) {
+      setResult(res.message);
+    } else {
+      setResult(res.message);
+    }
+  };
   return (
-    <section
-      id="contact"
-      className=" sm:max-w-[70%] sm:mx-auto lg:flex lg:mt-16 lg:gap-16 lg:items-center"
-    >
-      <h3 className="text-center text-secondary mt-12 mb-8 font-bold text-xl md:text-2xl lg:mt-0 px-4 sm:px-0 xl:text-3xl font-PrimaryF lg:font-SecondaryF">
-        Have you got a project in mind? Let&#39;s talk
-      </h3>
-      <form
-        action="https://formsubmit.co/umbertonardiello@gmail.com"
-        method="POST"
-        className="w-4/5 mx-auto rounded-lg px-8 py-4 mb-12 shadow-3xl sm:max-w-[65%] lg:max-w-[50%]"
-      >
-        <label
-          htmlFor="name"
-          className="text-secondary pl-2 font-PrimaryF relative"
-        >
-          <input
-            id="name"
-            type="text"
-            placeholder="Name"
-            name="name"
-            className=" bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-2 text-secondary focus:outline-none placeholder:text-secondary
-            placeholder:font-PrimaryF"
-          />
-        </label>
-        <label
-          htmlFor="email"
-          className="text-secondary pl-2 font-PrimaryF -translate-x-8"
-        >
-          <input
-            id="email"
-            type="text"
-            placeholder="Email"
-            name="email"
-            className=" bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-2 text-secondary focus:outline-none placeholder:text-secondary
-            placeholder:font-PrimaryF"
-          />
-        </label>
-        <TextareaAutosize
-          minRows={3}
-          maxRows={10}
-          id="textarea"
-          className="bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-5 text-secondary focus:outline-none placeholder:text-secondary
-          placeholder:font-PrimaryF mt-8"
-          placeholder="Message"
-        />
-        <input
-          type="submit"
-          value="Send"
-          className="text-secondary bg-transparent border-2 border-secondary px-8 py-1 rounded-lg block mx-auto hover:bg-secondary hover:text-additional
-          transition-all duration-300 font-PrimaryF mb-8"
-        />
-      </form>
-    </section>
-    // <div className=" border-2 border-solid border-secondary flex">
-    //   <form onSubmit={onSubmit}>
-    //     <input
-    //       type="text"
-    //       name="name"
-    //       className="block border-2 border-secondary"
-    //       placeholder="Name"
+    // <section
+    //   id="contact"
+    //   className=" sm:max-w-[70%] sm:mx-auto lg:flex lg:mt-16 lg:gap-16 lg:items-center"
+    // >
+    //   <h3 className="text-center text-secondary mt-12 mb-8 font-bold text-xl md:text-2xl lg:mt-0 px-4 sm:px-0 xl:text-3xl font-PrimaryF lg:font-SecondaryF">
+    //     Have you got a project in mind? Let&#39;s talk
+    //   </h3>
+    //   <form
+    //     action="https://formsubmit.co/umbertonardiello@gmail.com"
+    //     method="POST"
+    //     className="w-4/5 mx-auto rounded-lg px-8 py-4 mb-12 shadow-3xl sm:max-w-[65%] lg:max-w-[50%]"
+    //   >
+    //     <label
+    //       htmlFor="name"
+    //       className="text-secondary pl-2 font-PrimaryF relative"
+    //     >
+    //       <input
+    //         id="name"
+    //         type="text"
+    //         placeholder="Name"
+    //         name="name"
+    //         className=" bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-2 text-secondary focus:outline-none placeholder:text-secondary
+    //         placeholder:font-PrimaryF"
+    //       />
+    //     </label>
+    //     <label
+    //       htmlFor="email"
+    //       className="text-secondary pl-2 font-PrimaryF -translate-x-8"
+    //     >
+    //       <input
+    //         id="email"
+    //         type="text"
+    //         placeholder="Email"
+    //         name="email"
+    //         className=" bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-2 text-secondary focus:outline-none placeholder:text-secondary
+    //         placeholder:font-PrimaryF"
+    //       />
+    //     </label>
+    //     <TextareaAutosize
+    //       minRows={3}
+    //       maxRows={10}
+    //       id="textarea"
+    //       className="bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-5 text-secondary focus:outline-none placeholder:text-secondary
+    //       placeholder:font-PrimaryF mt-8"
+    //       placeholder="Message"
     //     />
     //     <input
-    //       type="email"
-    //       name="email"
-    //       className="block border-2 border-secondary"
-    //       placeholder="Email"
+    //       type="submit"
+    //       value="Send"
+    //       className="text-secondary bg-transparent border-2 border-secondary px-8 py-1 rounded-lg block mx-auto hover:bg-secondary hover:text-additional
+    //       transition-all duration-300 font-PrimaryF mb-8"
     //     />
-    //     <textarea
-    //       name="Text"
-    //       className="block border-2 border-secondary"
-    //       placeholder="Text"
-    //     ></textarea>
-    //     <input type="submit" className="block border-2 border-secondary" />
     //   </form>
-    //   <span>{result}</span>
-    // </div>
+    // </section>
+    <section id="contact">
+      <div className=" sm:max-w-[70%] sm:mx-auto lg:flex lg:mt-16 lg:gap-16 lg:items-center">
+        <h3 className="text-center text-secondary mt-12 mb-8 font-bold text-xl md:text-2xl lg:mt-0 px-4 sm:px-0 xl:text-3xl font-PrimaryF lg:font-SecondaryF">
+          Have you got a project in mind? Let&#39;s talk
+        </h3>
+        <form
+          onSubmit={onSubmit}
+          className="w-4/5 mx-auto rounded-lg px-8 py-4 mb-12 shadow-3xl sm:max-w-[65%] lg:max-w-[50%]"
+        >
+          <label
+            htmlFor="name"
+            className="text-secondary pl-2 font-PrimaryF relative"
+          >
+            <input
+              type="text"
+              name="name"
+              className=" bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-2 text-secondary focus:outline-none placeholder:text-secondary
+              //         placeholder:font-PrimaryF"
+              placeholder="Name"
+              required
+            />
+          </label>
+
+          <label
+            htmlFor="email"
+            className="text-secondary pl-2 font-PrimaryF -translate-x-8"
+          >
+            <input
+              type="email"
+              name="email"
+              className=" bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-2 text-secondary focus:outline-none placeholder:text-secondary
+              //         placeholder:font-PrimaryF"
+              placeholder="Email"
+              required
+            />
+          </label>
+          <textarea
+            name="Text"
+            className="bg-transparent block border-b border-b-secondary w-full pl-2 pb-2 mb-5 text-secondary focus:outline-none placeholder:text-secondary placeholder:font-PrimaryF mt-8"
+            placeholder="Message"
+          ></textarea>
+          <input
+            type="submit"
+            className="text-secondary bg-transparent border-2 border-secondary px-8 py-1 rounded-lg block mx-auto hover:bg-secondary hover:text-additional
+    //       transition-all duration-300 font-PrimaryF mb-8"
+          />
+          <span className=" text-secondary mx-auto text-center block">
+            {result}
+          </span>
+        </form>
+      </div>
+    </section>
   );
 };
 
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,47 +4,6 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 
 const HeroSection = () => {
-  // const frontend = new SplitType("#frontend");
-  // const app = useRef();
-  // const webDev = new SplitType("#webdev");
-  // const name = useRef();
-
-  // useLayoutEffect(() => {
-  //   gsap.from(frontend.chars, {
-  //     y: -100,
-  //     opacity: 1,
-  //     stagger: 0.2,
-  //     duration: 0.5,
-  //   });
-  //   gsap.from(webDev.chars, {
-  //     y: 100,
-  //     opacity: 1,
-  //     stagger: 0.2,
-  //     duration: 0.5,
-  //   });
-  //   gsap.from(name.current, { opacity: 0, duration: 2 });
-  // });
-
-  // const frontend = new SplitType("#frontend", { types: "words, chars" });
-  // const webDev = new SplitType("#webdev", { types: "words, chars" });
-  // const name = useRef();
-
-  // useLayoutEffect(() => {
-  //   gsap.from(frontend.chars, {
-  //     y: -100,
-  //     opacity: 1,
-  //     stagger: 0.2,
-  //     duration: 0.5,
-  //   });
-  //   gsap.from(webDev.chars, {
-  //     y: 100,
-  //     opacity: 1,
-  //     stagger: 0.2,
-  //     duration: 0.5,
-  //   });
-  //   gsap.to(name.current, { opacity: 1, duration: 2 });
-  // });
-
   const name = useRef();
   const frontend = "Frontend";
   const webDev = "Web Dev";
@@ -76,7 +35,7 @@ const HeroSection = () => {
   return (
     <>
       <div className="h-[calc(100vh-5.25rem)] flex justify-center items-center font-Ubuntu relative">
-        <div className='before:content-["UN"] before:text-[13rem] before:text-[#0d0d0d] before:absolute before:top-[50%] before:left-[50%] before:-translate-x-1/2 before:-translate-y-1/2 before:font-bold before:z-0 before:drop-shadow-xl before:blur-[1px] before:[text-shadow:_0_0_40px_rgb(235,89,57)] before:animate-glow sm:before:text-[20rem] before:font-SecondaryF lg:before:text-[25rem] '></div>
+        <div className="hero-section"></div>
         <div className="w-4/5 z-20 flex justify-center items-center relative">
           <div className="absolute flex justify-center items-center -left-4 mb-4 sm:mb-16 lg:mb-32 clip1">
             {frontend.split("").map((letter, i) => {
