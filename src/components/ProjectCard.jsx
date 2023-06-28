b1ea4e4c1cd4591a527c92da36571ed0682b0b2a
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-06-28 08:38:41 +0100

    project card created

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,38 @@
-// import React from "react";
 import { LuGithub, LuArrowRight } from "react-icons/lu";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useEffect } from "react";
 
 const ProjectCard = (props) => {
   const windowWidth = window.innerWidth;
-  const id = props.id;
+  const { image, title, description, link, repo, id } = props;
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const triggers = document.querySelectorAll(".projects-container");
+    triggers.forEach((container) => {
+      let image = container.querySelector(".projectImg");
+      let description = container.querySelector(".description");
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: container,
+          start: "top: 95%",
+        },
+      });
+
+      tl.fromTo(
+        image,
+        { y: -100, autoAlpha: 0 },
+        { y: 0, autoAlpha: 1, duration: 1.5, ease: "slow(0.7, 0.7, false)" }
+      );
+
+      tl.fromTo(
+        description,
+        { y: 100, autoAlpha: 0 },
+        { y: 0, autoAlpha: 1, duration: 1.5, ease: "power1.inOut", delay: -1.5 }
+      );
+    });
+  });
   return (
     <div
       className={`${id % 2 === 0 && "flex-row-reverse"} ${
@@ -11,24 +40,24 @@ const ProjectCard = (props) => {
       } projects-container`}
     >
       <img
-        src={props.image}
-        className="sm:h-100% projectImg w-full sm:w-[50%]"
-        alt={`Project${props.id} picture`}
+        src={image}
+        className="sm:h-100% projectImg w-full sm:w-[50%] opacity-0"
+        alt={`Project${id} picture`}
       ></img>
-      <div className="flexy flex-col px-4 text-center text-secondary sm:w-[50%] md:px-8 md: description">
+      <div className="flexy flex-col px-4 text-center text-secondary sm:w-[50%] md:px-8 opacity-0 description">
         <p className="text-md mb-4 font-bold uppercase lg:text-xl xl:pb-8 xl:text-2xl">
-          {props.title}
+          {title}
         </p>
         <p className="mb-4 font-PrimaryF text-sm text-slate-300 xl:pb-8 xl:text-lg">
-          {props.description}
+          {description}
         </p>
         <div className="flexy flex-col gap-4 md:flex-row md:gap-8">
-          <a href={props.link} target="_blank" rel="noreferrer">
+          <a href={link} target="_blank" rel="noreferrer">
             <button className="projects-button group">
               Link <LuArrowRight className="projects-button__icons" />
             </button>
           </a>
-          <a href={props.repo} target="_blank" rel="noreferrer">
+          <a href={repo} target="_blank" rel="noreferrer">
             <button className="projects-button group">
               GitHub
               <LuGithub className="projects-button__icons" />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,47 +1,15 @@
 import data from "../data/projectData";
-// import { LuGithub, LuArrowRight } from "react-icons/lu";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import { useEffect } from "react";
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
-  gsap.registerPlugin(ScrollTrigger);
-
-  useEffect(() => {
-    const triggers = document.querySelectorAll(".projects-container");
-    triggers.forEach((container) => {
-      let image = container.querySelector(".projectImg");
-      let description = container.querySelector(".description")
-      let tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: container,
-          start: "top: 95%",
-        },
-      });
-
-      tl.fromTo(
-        image,
-        { y: -100, opacity: 0 },
-        { y: 0, opacity: 1, duration: 1, ease: "slow(0.7, 0.7, false)" }
-      );
-
-      tl.fromTo(
-        description,
-        { y: 100, opacity: 0 },
-        { y: 0, opacity: 1, duration: 1, ease: "power1.inOut", delay: -1 }
-      );
-
-      
-    });
-  });
+  
 
   const card = data.map((data) => {
     return (
       <ProjectCard image={data.source} key={data.id} title={data.title} description={data.description} link={data.href} repo={data.repo} id={data.id}/>
     );
   });
-  
+
   return (
     <>
       <section
