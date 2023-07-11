b3e29844a4d094414ecd5ea80dafa97d2a360629
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-07-11 17:41:06 +0100

    form adjusted 1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
-import { motion } from "framer-motion";
-import React from "react";
+import React, { useRef, useLayoutEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 const Contact = () => {
   const [result, setResult] = React.useState("");
@@ -23,15 +24,31 @@ const Contact = () => {
     }
   };
 
+  const contacts = useRef(),
+    comp2 = useRef();
+  gsap.registerPlugin(ScrollTrigger);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.to(contacts.current, {
+        autoAlpha: 1,
+        duration: 1.5,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: contacts.current,
+          start: "top 90%",
+        },
+      });
+    }, comp2);
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <motion.section
-      id="contact"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-      transition={{ delay: 0.3 }}
-    >
-      <div className=" sm:max-w-[70%] sm:mx-auto lg:flex lg:mt-16 lg:gap-16 lg:items-center">
+    <section id="contact" ref={comp2}>
+      <div
+        className=" sm:max-w-[70%] sm:mx-auto lg:flex lg:mt-16 lg:gap-16 lg:items-center opacity-0"
+        ref={contacts}
+      >
         <h3 className="text-center text-secondary mt-12 mb-8 font-bold text-xl md:text-2xl lg:mt-0 px-4 sm:px-0 xl:text-3xl font-PrimaryF lg:font-SecondaryF">
           Have you got a project in mind? Let&#39;s talk
         </h3>
@@ -44,22 +61,14 @@ const Contact = () => {
             className="text-secondary pl-2 font-PrimaryF relative"
             aria-label="name"
           >
-            <motion.input
+            <input
               type="text"
               id="name"
               className="form-input"
               placeholder="Name"
               required
-<<<<<<< HEAD
               name="name"
             />
-=======
-              initial={{ opacity: 0, x: -100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: true }}
-              transition={{ delay: 0.6 }}
-            ></motion.input>
->>>>>>> b82d67febdc10f6ff4da630809bea95c75bf2ec2
           </label>
 
           <label
@@ -67,31 +76,23 @@ const Contact = () => {
             aria-label="email"
             className="text-secondary pl-2 font-PrimaryF -translate-x-8"
           >
-            <motion.input
+            <input
               type="email"
               id="email"
               className="form-input"
               placeholder="Email"
               name="email"
               required
-              initial={{ opacity: 0, x: 100 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: true }}
-              transition={{ delay: 0.7 }}
-            ></motion.input>
+            />
           </label>
 
           <label htmlFor="textarea" aria-label="textaread">
-            <motion.textarea
+            <textarea
               id="textarea"
               name="Text"
               className="form-input mt-8"
               placeholder="Message"
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
-              transition={{ delay: 1 }}
-            ></motion.textarea>
+            ></textarea>
           </label>
           <input
             type="submit"
@@ -104,7 +105,7 @@ const Contact = () => {
           </span>
         </form>
       </div>
-    </motion.section>
+    </section>
   );
 };
 
